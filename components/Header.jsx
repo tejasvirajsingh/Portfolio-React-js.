ea1d7b21efddbf3a6becf174854ba75b24765ae0
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-12-28 23:48:22 +0530

    fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         <div className="hidden xl:flex items-center gap-6">
           <Nav />
           <Link href="/contact">
-            <Button>Reach Me</Button>
+            <Button>Reach Mee</Button>
           </Link>
         </div>
 
